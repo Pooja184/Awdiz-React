5024be9a9a27b9769683507e393639ee474d75a3
pxxja <poojabhambid2004@gmail.com>
2025-07-27 20:14:06 +0530

    react Assignment Day 2

---
+++ Day2-27-07/src/App.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import '../src/App.css'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './component/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+
+     </Routes>
+    </>
+  )
+}
+
+export default App
