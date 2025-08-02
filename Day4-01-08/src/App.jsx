434fd17d23622c3e70ef28c143ad55df10897726
pxxja <poojabhambid2004@gmail.com>
2025-08-02 19:43:29 +0530

    create vite app

---
+++ Day4-01-08/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Todo from './components/Todo'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Todo/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
