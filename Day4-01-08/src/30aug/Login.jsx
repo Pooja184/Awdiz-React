4f5656661362552df14bda79eee0f2a9043bf679
pxxja <poojabhambid2004@gmail.com>
2025-11-01 23:11:24 +0530

    completed backend assignment

--- Day4-01-08/src/30aug/Login.jsx
+++ Day4-01-08/src/30aug/Login.jsx
@@ -1,46 +1,68 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux";
+import { userLogin } from "../23aug/features/calculator";
 
+import axios from 'axios'
 const Login = () => {
+    const { user } = useSelector((state) => state.calculator);
+
+  console.log("selected user:", user)
+  const dispatch=useDispatch()
   const [userInfo,setUserInfo]=useState({
     
-    username:"",
+    email:"",
     password:""
   })
-  const [formData,setFormData]=useState([])
   const handleChange=(e)=>{
     // console.log({[e.target.name]:e.target.value})
     setUserInfo({...userInfo,[e.target.name]:e.target.value})
     // console.log(userInfo)
   }
 
-  const handleSubmit=()=>{
-    // console.log(userInfo)
-    setFormData([...formData,userInfo])
-    // console.log([...formData,userInfo])
-    const userData=JSON.parse(localStorage.getItem("users"))
-    let isValid=false;
-    for(let user of userData){
-        if(user.username == userInfo.username && user.password ==userInfo.password){
-            isValid=true;
-            break;
-        }
-    }
-     if (isValid) {
-        alert("You've successfully login")
-    setUserInfo({username:"",password:""})
-
-    }else{
-        alert("Please check your credentials")
-    }
-    // console.log(userData)
+  const handleSubmit=async()=>{
+   try {
+     if ( !userInfo.email || !userInfo.password) {
+       alert("Please fill in all required fields");
+       return;
+     }
+     
+       const response = await axios.post("http://localhost:8000/api/v1/auth/Login", 
+         userInfo,
+       );
+       console.log("login response:", response.data);
+       // dispatch the user into redux using the action creator
+       // response.data may contain the user object or a message/token depending on your backend
+       dispatch(userLogin(response.data));
+       console.log("dispatched user to store");
+       if (response.data) {
+         alert("login successful!");
+       }
+     setUserInfo({
+       
+       email: "",
+       password: ""
+     })
+   } catch (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          alert(error.response.data.message || "Registration failed. Please try again.");
+          console.log("Error data:", error.response.data);
+        } 
+   }
   }
   return (
-    <div className='flex justify-center items-center  '>
+  <div>
+      <h1>Hello, {user ? user.user.name :"..."}</h1>
+
+      <div className='flex justify-center items-center  '>
       <div className=' border-4 border-black p-10 bg-red-400 '>
        
+       
         <div className='text-left mt-2'>
-          <label htmlFor="Username">Username</label><br />
-        <input onChange={handleChange} className='outline-none' type="text" name="username" value={userInfo.username} id="Username" /><br />
+          <label htmlFor="Username">email</label><br />
+        <input onChange={handleChange} className='outline-none' type="email" name="email" value={userInfo.email} id="email" /><br />
         </div>
         <div className='text-left mt-2'>
             <label htmlFor="password">Password</label><br />
@@ -49,6 +71,7 @@ const Login = () => {
        <button onClick={handleSubmit} className='bg-green-700 px-4 py-2 mt-2'>Submit</button>
       </div>
     </div>
+  </div>
   )
 }
 
